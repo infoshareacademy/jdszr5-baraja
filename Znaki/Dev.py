ad7ee3dc49f76dfcfbbc8b19b62b8cdfc0b62b5d
Bartosz Zochniak <bartosz.zochniak@aiesec.net>
2022-04-02 12:37:43 +0200

    random photos added and evaluated

--- Znaki/Dev.py
+++ Znaki/Dev.py
@@ -1,8 +1,14 @@
-import pickle
-import tenserflow
+from keras.models import load_model
+import cv2
+import matplotlib.pyplot as plt
+
+model = load_model("model_weights.h5")
+
+imp = cv2.imread("stop.jpg")
+
+plt.imshow(imp)
+plt.show()
 
 
-with open("model", "rb") as m:
-    model = pickle.load(m)
-
+print(model.predict(imp))
 
